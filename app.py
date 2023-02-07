0fd0edb19963b898e25445c159540b0f10dbb682
destiny3912 <destiny3910@gmail.com>
2023-02-07 10:39:38 -0800

    file flow:

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, request, send_file
 
 app = Flask(__name__)
 
 
+@app.route("/api/predict/", methods=['POST'])
+def predict_sound():
+    text = request.json
+
+    path = "./temp/"
+    return send_file(path + "notifications-sound-127856.mp3",
+                     "multipart/form-data", as_attachment=True)
+
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return "Hello World"
 
 
 if __name__ == '__main__':
