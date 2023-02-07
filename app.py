fb57b61cff40167f470fa5cdcc8d397dc39e3467
destiny3912 <destiny3910@gmail.com>
2023-02-07 14:08:59 -0800

    CORS

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, request, send_file
+from flask_cors import CORS
+import requests
 import os
 
 app = Flask(__name__)
-
+CORS(app)
 
 def model_request(text):
     # TODO 모델 가져와서 텍스트를 input으로 output으로 파일이름
     return "notifications-sound-127856.mp3"
 
 
+def service_request(file_name, jwt):
+    target = open(file_name, "r")
+    response = requests.post(url="https://www.ljhhosting.com/api/sound/upload", files=target, headers={'X-ACCESS-TOKEN': jwt})
+
+    return response
+
+
 @app.route("/api/predict/", methods=['POST'])
 def predict_sound():
     text = request.json['sentence']
+    jwt = request.headers['X-ACCESS-TOKEN']
 
     file_name = model_request(text)
     path = "./temp/"
